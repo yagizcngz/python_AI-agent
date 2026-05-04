5182a576e0eefebf8ed138862a7df8bef3bdc835
Cengiz <yagizcengiz888@gmail.com>
2026-05-04 15:17:32 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -15,12 +15,32 @@ def main():
 
     if not API_KEY:
         raise RuntimeError("OPENROUTER_API_KEY is not set")
+    
 
     client = OpenAI(api_key=API_KEY, base_url=BASE_URL)
 
     chat = client.chat.completions.create(
         model="anthropic/claude-haiku-4.5",
         messages=[{"role": "user", "content": args.p}],
+        tools=[
+            {
+  "type": "function",
+  "function": {
+    "name": "Read",
+    "description": "Read and return the contents of a file",
+    "parameters": {
+      "type": "object",
+      "properties": {
+        "file_path": {
+          "type": "string",
+          "description": "The path to the file to read"
+        }
+      },
+      "required": ["file_path"]
+    }
+  }
+}
+        ],
     )
 
     if not chat.choices or len(chat.choices) == 0:
